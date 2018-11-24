2c6d067d15c4a1f11a9fad0aea33991fa6c1c868
M7mdAmr3 <43708078+M7mdAmr3@users.noreply.github.com>
2018-11-24 09:17:19 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,28 @@
+﻿const Discord = require("discord.js");
+const client = new Discord.Client();
+
+
+
+client.on("ready", () => {
+console.log('By : m7md');
+client.user.setPresence({
+  status: 'dnd',
+  game: { 
+     type: 0,
+     name: 'Fortnite',
+     details: `Battle Royal - In Lobby`,
+     url: 'https://www.twitch.com/xdddd23',
+     state: `Playing Solo (1 of 1)`,
+    application_id: '502663260571697168',
+     assets: {
+        small_image: `511551607071309824`,
+        small_text: 'Tier 86',
+        large_image: `511551570677202955`,
+        large_text: `Fortnite` }
+
+  }
+    });
+});
+
+
+client.login("NDAxNDE2Mzg0MjE2Njk0Nzg1.DtpUpQ.TsHAQBrrW2P6ClRkP7e1iTnHk-c");
